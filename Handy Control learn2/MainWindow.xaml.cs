978ad0bd91288c7bff8abf973da8f9346a04e57a
adarshrise <adarshrise20@gmail.com>
2019-11-24 07:49:37 +0530

    Added MessageBox Override Test Button

--- Handy Control learn2/MainWindow.xaml.cs
+++ Handy Control learn2/MainWindow.xaml.cs
@@ -1,10 +1,66 @@
-﻿namespace Handy_Control_learn2
+﻿
+
+
+using System;
+
+using HandyControl.Controls;
+using HandyControl.Data;
+using System.Windows;
+using MessageBox = HandyControl.Controls.MessageBox;
+//using MessageBox = System.Windows.MessageBox;
+
+namespace Handy_Control_learn2
 {
     public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void But_Min_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            System.Windows.MessageBox.Show("this is great");
+            MessageBox.Ask("This came here");
+         
+
+            /*
+            Growl.Info(new GrowlInfo
+            {
+                Message = "we found new version",
+                CancelStr = "later",
+                ConfirmStr = "lets do this"
+            });
+            */
+        }
+
+        private void But_Close_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            MessageBox.Ask("This came here");
+            /*
+            Growl.Info(new GrowlInfo
+            {
+                Message = "we found new version",
+                CancelStr = "later",
+                ConfirmStr = "lets do this"
+            });
+            */
+        }
+
+        private void Test_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            // Message = "Test", Caption = "Url", Button = MessageBoxButton.YesNo, IconBrushKey = ResourceToken.AccentBrush, IconKey = ResourceToken.InfoGeometry, StyleKey = "MessageBoxCustom"
+
+            MessageBox.Show(new MessageBoxInfo
+            {
+                Message = "show this",
+                Caption = "Title",
+                Button = MessageBoxButton.YesNo,
+                IconBrushKey = ResourceToken.AccentBrush,
+                IconKey = ResourceToken.AskGeometry,
+                StyleKey = "MessageBoxCustom"
+            });
+
         }
     }
 }
